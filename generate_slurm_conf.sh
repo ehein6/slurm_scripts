d1b9b82800945db689ee34576148479abc8a6356
Eric Hein <ehein6@gatech.edu>
2017-09-27 13:45:11 -0400

    Use the 'all' partition

--- generate_slurm_conf.sh
+++ generate_slurm_conf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-sinfo --Node --noheader --format="%N" --responding -p debug |
+sinfo --Node --noheader --format="%N" --responding -p all |
 while read -r node; do
     srun -i /dev/null --nodelist ${node} ./get_slurm_conf.py slurm.conf
 done
